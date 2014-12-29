e756aa04caae83d485a71a7cc5ae46f013ae0e3b
deniz <d.kadio@gmail.com>
2014-12-29 14:01:49 +0100

    merge

---
+++ Ausarbeitung/amiede-htwsaar-i-mst-vorlage-9eb5af137b5a/Chapters/Beispiele/clear.bat
@@ -0,0 +1,18 @@
+del *.aux
+del *.toc
+del *.out
+del *.bak
+del *.brf
+del *.blg
+del *.bbl
+del *.lof
+del *.lot
+del *.lol
+del *.dvi
+del *.idx
+del *.ind
+del *.glo
+del *.gls
+del *.tdo
+del *.log
+
